e7eb5ef75503dc81271605e77f288be27fa03537
jaejeonglee <https://github.com/jaejeonglee>
2022-02-19 10:02:26 +0900

    전체 게시물조회  API

--- routes/post.js
+++ routes/post.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const Post = require('../models/post')
 const auth = require("../middlewares/auth-middleware");
 const cors = require('cors');
+const res = require('express/lib/response');
 
 router.post('/', async (req, res) => { // 게시물 작성
     const {  userId,title, tag, contents, thumbnail, introduce  } = req.body;
@@ -21,5 +22,8 @@ router.post('/', async (req, res) => { // 게시물 작성
     res.status(200).json({ mssage: "게시물이 저장되었습니다."});
 });
 
-
+router.get('/', async (req, res) => {
+    const posts = await Post.find({})
+    res.status(200).json({ posts })
+})
 module.exports = router;
